1cb05c1e5cced2d20b3a2e2b2dfc0d89178f3b29
saifullah rahman <saiflll284@gmail.com>
2022-01-22 16:15:25 +0530

    added styles to header component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-function App() {
-    
-}
+import Header from "./components/Header";
 
+function App() {
+  return (
+    <>
+      <Header text="Feedback UI" />
+      <div className="container">
+        <h1>My App</h1>
+      </div>
+    </>
+  );
+}
 
 export default App;
